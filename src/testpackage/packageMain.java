b2271a662b0e0caa06974c57fea9c4a33a5e8329
andrey019 <andrey019@gmail.com>
2016-04-08 18:03:20 +0300

    commit2

---
+++ src/testpackage/packageMain.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+/**
+ * Created by andrey on 08.04.16.
+ */
+public class packageMain {
+    public static void main(String[] args){
+        System.out.print("sldkgj!");
+    }
+}
